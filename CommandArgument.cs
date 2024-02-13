83b2c962653c0525f67cbc4ff4f920cc7abc8cc9
JohnP55 <johnp55@mkw.re>
2024-02-13 12:56:57 -0500

    Initial commit

---
+++ CommandArgument.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JP55.CmdParser
+{
+    public enum CommandArgumentType
+    {
+        FLAG,
+        VALUE,
+        KEY_VALUE
+    }
+    public class CommandArgument
+    {
+        public string Name { get; }
+        public CommandArgumentType Type { get; }
+        public int ArgCount { get; }
+
+        public CommandArgument(string name, CommandArgumentType type, int argCount)
+        {
+            Name = name;
+            Type = type;
+            ArgCount = argCount;
+        }
+    }
+}
---
+++ CommandManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JP55.CmdParser
+{
+    public class CommandNotFoundException : Exception
+    {
+        public CommandNotFoundException() : base("The specified command was not found in the command manager.") { }
+    }
+    public class CommandManager
+    {
+        public List<CommandBase> Commands { get; } = new List<CommandBase>();
+        public CommandManager() { }
+        public CommandManager(params CommandBase[] commands)
+        {
+            Commands.AddRange(commands);
+        }
+        public void AddCommand(CommandBase command)
+        {
+            Commands.Add(command);
+        }
+        public string HelpMsg()
+        {
+            if (Commands.Count == 0) return "No commands.";
+            List<string> individualCommands = new List<string>();
+            foreach (var command in Commands)
+            {
+                individualCommands.Add($"{command.Name}: {command.Syntax}{Environment.NewLine}\t{command.HelpMessage}");
+            }
+            return string.Join($"{Environment.NewLine}{Environment.NewLine}", individualCommands);
+        }
+        public CommandBase? Parse(IEnumerable<string> args)
+        {
+            if (args.Count() == 0) return null;
+            string name = args.First();
+            foreach (var command in Commands)
+            {
+                if (command.Name == name)
+                    return command;
+            }
+            
+            return null;
+        }
+        public TOut Execute<TOut>(CommandBase command, IEnumerable<string> args)
+        {
+            var method = command.GetType().GetMethod("Execute", new[] { typeof(IEnumerable<string>) })!;
+            return (TOut)method.Invoke(command, new[] { args })!;
+        }
+    }
+}
